08efbf4eea6eccaa94c8905a2ec1283cde0b87fa
Ajdin Buljubasic <ajdin-2000@outlook.com>
2024-06-10 22:09:23 +0100

    mobile view fix maybe

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\Ajdin\AppData\Local\Temp\tmpsb1q5fuu.js
+// include: C:\Users\Ajdin\AppData\Local\Temp\tmpc6dtfcy0.js
 
   if (!Module.expectedDataFileDownloads) {
     Module.expectedDataFileDownloads = 0;
@@ -219,21 +219,21 @@ var REMOTE_PACKAGE_SIZE = metadata['remote_package_size'];
 
   })();
 
-// end include: C:\Users\Ajdin\AppData\Local\Temp\tmpsb1q5fuu.js
-// include: C:\Users\Ajdin\AppData\Local\Temp\tmpgpimtou0.js
+// end include: C:\Users\Ajdin\AppData\Local\Temp\tmpc6dtfcy0.js
+// include: C:\Users\Ajdin\AppData\Local\Temp\tmpbw2e13u4.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['$ww'] || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: C:\Users\Ajdin\AppData\Local\Temp\tmpgpimtou0.js
-// include: C:\Users\Ajdin\AppData\Local\Temp\tmpnal9q_nw.js
+  // end include: C:\Users\Ajdin\AppData\Local\Temp\tmpbw2e13u4.js
+// include: C:\Users\Ajdin\AppData\Local\Temp\tmpvgome0iu.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: C:\Users\Ajdin\AppData\Local\Temp\tmpnal9q_nw.js
+  // end include: C:\Users\Ajdin\AppData\Local\Temp\tmpvgome0iu.js
 
 
 // Sometimes an existing Module object exists with properties
