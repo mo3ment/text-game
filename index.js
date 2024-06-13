35edbbd7fea9de29e976cbb6d9c69ac7b7a64269
Ajdin Buljubasic <ajdin-2000@outlook.com>
2024-06-13 23:22:32 +0100

    cut mid sentence functionality

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\Ajdin\AppData\Local\Temp\tmp4ejzs2_r.js
+// include: C:\Users\Ajdin\AppData\Local\Temp\tmps091zwru.js
 
   if (!Module.expectedDataFileDownloads) {
     Module.expectedDataFileDownloads = 0;
@@ -219,21 +219,21 @@ var REMOTE_PACKAGE_SIZE = metadata['remote_package_size'];
 
   })();
 
-// end include: C:\Users\Ajdin\AppData\Local\Temp\tmp4ejzs2_r.js
-// include: C:\Users\Ajdin\AppData\Local\Temp\tmpfvujufd6.js
+// end include: C:\Users\Ajdin\AppData\Local\Temp\tmps091zwru.js
+// include: C:\Users\Ajdin\AppData\Local\Temp\tmpw8gwnqk1.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['$ww'] || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: C:\Users\Ajdin\AppData\Local\Temp\tmpfvujufd6.js
-// include: C:\Users\Ajdin\AppData\Local\Temp\tmp5jnnl48f.js
+  // end include: C:\Users\Ajdin\AppData\Local\Temp\tmpw8gwnqk1.js
+// include: C:\Users\Ajdin\AppData\Local\Temp\tmpczuppzvt.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: C:\Users\Ajdin\AppData\Local\Temp\tmp5jnnl48f.js
+  // end include: C:\Users\Ajdin\AppData\Local\Temp\tmpczuppzvt.js
 
 
 // Sometimes an existing Module object exists with properties
@@ -1133,40 +1133,40 @@ function dbg(...args) {
 // === Body ===
 
 var ASM_CONSTS = {
-  96464: () => { if (document.fullscreenElement) return 1; },  
- 96510: () => { return document.getElementById('canvas').width; },  
- 96562: () => { return parseInt(document.getElementById('canvas').style.width); },  
- 96630: () => { document.exitFullscreen(); },  
- 96657: () => { setTimeout(function() { Module.requestFullscreen(false, false); }, 100); },  
- 96730: () => { if (document.fullscreenElement) return 1; },  
- 96776: () => { return document.getElementById('canvas').width; },  
- 96828: () => { return screen.width; },  
- 96853: () => { document.exitFullscreen(); },  
- 96880: () => { setTimeout(function() { Module.requestFullscreen(false, true); setTimeout(function() { canvas.style.width="unset"; }, 100); }, 100); },  
- 97013: () => { if (document.fullscreenElement) return 1; },  
- 97059: () => { return document.getElementById('canvas').width; },  
- 97111: () => { return parseInt(document.getElementById('canvas').style.width); },  
- 97179: () => { if (document.fullscreenElement) return 1; },  
- 97225: () => { return document.getElementById('canvas').width; },  
- 97277: () => { return screen.width; },  
- 97302: () => { if (document.fullscreenElement) return 1; },  
- 97348: () => { return document.getElementById('canvas').width; },  
- 97400: () => { return screen.width; },  
- 97425: () => { document.exitFullscreen(); },  
- 97452: () => { if (document.fullscreenElement) return 1; },  
- 97498: () => { return document.getElementById('canvas').width; },  
- 97550: () => { return parseInt(document.getElementById('canvas').style.width); },  
- 97618: () => { document.exitFullscreen(); },  
- 97645: () => { return screen.width; },  
- 97670: () => { return screen.height; },  
- 97696: () => { return window.screenX; },  
- 97723: () => { return window.screenY; },  
- 97750: ($0) => { navigator.clipboard.writeText(UTF8ToString($0)); },  
- 97803: ($0) => { document.getElementById("canvas").style.cursor = UTF8ToString($0); },  
- 97874: () => { document.getElementById('canvas').style.cursor = 'none'; },  
- 97931: ($0) => { document.getElementById('canvas').style.cursor = UTF8ToString($0); },  
- 98002: () => { if (document.fullscreenElement) return 1; },  
- 98048: () => { if (document.pointerLockElement) return 1; }
+  96240: () => { if (document.fullscreenElement) return 1; },  
+ 96286: () => { return document.getElementById('canvas').width; },  
+ 96338: () => { return parseInt(document.getElementById('canvas').style.width); },  
+ 96406: () => { document.exitFullscreen(); },  
+ 96433: () => { setTimeout(function() { Module.requestFullscreen(false, false); }, 100); },  
+ 96506: () => { if (document.fullscreenElement) return 1; },  
+ 96552: () => { return document.getElementById('canvas').width; },  
+ 96604: () => { return screen.width; },  
+ 96629: () => { document.exitFullscreen(); },  
+ 96656: () => { setTimeout(function() { Module.requestFullscreen(false, true); setTimeout(function() { canvas.style.width="unset"; }, 100); }, 100); },  
+ 96789: () => { if (document.fullscreenElement) return 1; },  
+ 96835: () => { return document.getElementById('canvas').width; },  
+ 96887: () => { return parseInt(document.getElementById('canvas').style.width); },  
+ 96955: () => { if (document.fullscreenElement) return 1; },  
+ 97001: () => { return document.getElementById('canvas').width; },  
+ 97053: () => { return screen.width; },  
+ 97078: () => { if (document.fullscreenElement) return 1; },  
+ 97124: () => { return document.getElementById('canvas').width; },  
+ 97176: () => { return screen.width; },  
+ 97201: () => { document.exitFullscreen(); },  
+ 97228: () => { if (document.fullscreenElement) return 1; },  
+ 97274: () => { return document.getElementById('canvas').width; },  
+ 97326: () => { return parseInt(document.getElementById('canvas').style.width); },  
+ 97394: () => { document.exitFullscreen(); },  
+ 97421: () => { return screen.width; },  
+ 97446: () => { return screen.height; },  
+ 97472: () => { return window.screenX; },  
+ 97499: () => { return window.screenY; },  
+ 97526: ($0) => { navigator.clipboard.writeText(UTF8ToString($0)); },  
+ 97579: ($0) => { document.getElementById("canvas").style.cursor = UTF8ToString($0); },  
+ 97650: () => { document.getElementById('canvas').style.cursor = 'none'; },  
+ 97707: ($0) => { document.getElementById('canvas').style.cursor = UTF8ToString($0); },  
+ 97778: () => { if (document.fullscreenElement) return 1; },  
+ 97824: () => { if (document.pointerLockElement) return 1; }
 };
 function GetWindowInnerWidth() { return window.innerWidth; }
 function GetWindowInnerHeight() { return window.innerHeight; }
